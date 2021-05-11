99f43aa3f47fcb9415ff1ec282509fbaaf6b98a7
Mateus Interciso <minterciso@deloitte.com>
2021-05-11 00:06:44 -0300

    * First complete version

---
+++ README.md
@@ -0,0 +1,8 @@
+This is my first attempt at "ricing" a i3-gaps.
+
+You'll need:
+
+* i3-gaps
+* MonoSpace Nerd Font
+* Polybar
+* Conky
---
+++ config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Terminate any currently running instances
+killall -q polybar
+
+# Pause while killall completes
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+polybar --reload main -c ~/.config/polybar/config &
